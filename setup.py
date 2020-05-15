bf9c221e9ac095f664f227b892ec5662d2ac7417
G. Ryan Sablosky <sablosky@psc.edu>
2020-05-15 15:39:53 -0400

    Add dateutil dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version='0.1',
     packages=['amieclient'],
     install_requires=[
-        'requests>=2.23.0'
+        'requests>=2.23.0',
+        'dateutil>=2.8.1'
     ],
     author='G. Ryan Sablosky',
     author_email='sablosky@psc.edu',
