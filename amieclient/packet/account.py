42b2fc1c63cf0197cc7040d0e3adb7ee76f3e47f
G. Ryan Sablosky <sablosky@psc.edu>
2020-05-26 13:14:27 -0400

    Add remaining account packet info

--- amieclient/packet/account.py
+++ amieclient/packet/account.py
@@ -1,18 +1,122 @@
 """
 AMIE packets relating to accounts
-
-notify_account_create
-notify_account_inactivate 
-notify_account_reactivate 
-request_account_create 
-request_account_inactivate
-request_account_reactivate 
 """
 
 from . import Packet
+
 
 class DataAccountCreate(Packet):
     _packet_type = 'data_account_create'
     _expected_reply = ['inform_transaction_complete']
     _data_keys_required = ['PersonID', 'ProjectID']
     _data_keys_allowed = ['DnList']
+
+
+class NotifyAccountCreate(Packet):
+    _packet_type = 'notify_account_create'
+    _expected_reply = ['data_account_create']
+    _data_keys_required = [
+        'AccountActivityTime',
+        'AcademicDegree',
+        'ProjectID',
+        'ResourceList',
+        'UserFirstName',
+        'UserLastName',
+        'UserOrganization',
+        'UserOrgCode',
+        'UserPersonID',
+        'UserRemoteSiteLogin',
+    ]
+    _data_keys_allowed = [
+        'NsfStatusCode',
+        'RoleList',
+        'UserBusinessPhoneExtension',
+        'UserBusinessPhoneNumber',
+        'UserCity',
+        'UserCountry',
+        'UserDepartment',
+        'UserDnList',
+        'UserDnList',
+        'UserEmail',
+        'UserFax',
+        'UserGlobalID',
+        'UserHomePhoneExtension',
+        'UserHomePhoneNumber',
+        'UserMiddleName',
+        'UserPasswordAccessEnable',
+        'UserPosition',
+        'UserRequestedLoginList',
+        'UserState',
+        'UserStreetAddress',
+        'UserStreetAddress2',
+        'UserTitle',
+    ]
+
+
+class NotifyAccountInactivate(Packet):
+    _packet_type = 'notify_account_inactivate'
+    _expected_reply = ['inform_transaction_complete']
+    _data_keys_required = ['PersonID', 'ProjectID', 'ResourceList']
+    _data_keys_allowed = ['Comment']
+
+
+class NotifyAccountReactivate(Packet):
+    _packet_type = 'notify_account_reactivate'
+    _expected_reply = ['inform_transaction_complete']
+    _data_keys_required = ['PersonID', 'ProjectID', 'ResourceList']
+    _data_keys_allowed = ['Comment']
+
+
+class RequestAccountCreate(Packet):
+    _packet_type = 'request_account_create'
+    _expected_reply = ['notify_account_reactivate']
+    _data_keys_required = [
+        'GrantNumber',
+        'ResourceList',
+        'UserFirstName',
+        'UserLastName',
+        'UserOrganization',
+        'UserOrgCode',
+    ]
+    _data_keys_allowed = [
+        'NsfStatusCode',
+        'ProjectID',
+        'SitePersonId',
+        'AcademicDegree',
+        'CitizenshipList',
+        'UserBusinessPhoneComment',
+        'UserBusinessPhoneExtension',
+        'UserBusinessPhoneNumber',
+        'UserCity',
+        'UserCountry',
+        'UserDepartment',
+        'UserDnList',
+        'UserEmail',
+        'UserFax',
+        'UserHomePhoneComment',
+        'UserHomePhoneExtension',
+        'UserHomePhoneNumber',
+        'UserMiddleName',
+        'UserPasswordAccessEnable',
+        'UserPersonID',
+        'UserRequestedLoginList',
+        'UserState',
+        'UserStreetAddress',
+        'UserStreetAddress2',
+        'UserTitle',
+        'UserZip',
+    ]
+
+
+class RequestAccountInactivate(Packet):
+    _packet_type = 'request_account_inactivate'
+    _expected_reply = ['notify_account_inactivate']
+    _data_keys_required = ['PersonID', 'ProjectID', 'ResourceList']
+    _data_keys_allowed = ['Comment']
+
+
+class RequestAccountReactivate(Packet):
+    _packet_type = 'request_account_reactivate'
+    _expected_reply = ['notify_account_reactivate']
+    _data_keys_required = ['PersonID', 'ProjectID', 'ResourceList']
+    _data_keys_allowed = ['Comment']
