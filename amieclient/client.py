ea9c43190df41daebceed7723c4b04682a6ad0e4
G. Ryan Sablosky <sablosky@psc.edu>
2020-06-04 17:07:25 -0400

    flesh out client a bit more

--- amieclient/client.py
+++ amieclient/client.py
@@ -1,4 +1,20 @@
+import requests
+import dateutil
+from urlparse import urljoin
 """AMIE client class"""
+
+
+class ServerSession(requests.Session):
+    """
+    Recipe from https://github.com/psf/requests/issues/2554#issuecomment-109341010
+    """
+    def __init__(self, prefix_url):
+        self.prefix_url = prefix_url
+        super().__init__()
+
+    def request(self, method, url, *args, **kwargs):
+        url = urljoin(self.prefix_url, url)
+        return super().request(method, url, *args, **kwargs)
 
 
 class Client(object):
@@ -6,19 +22,38 @@ class Client(object):
     AMIE Client
 
     Example:
-    psc_client = amieclient.Client('https://amieclient.xsede.org/v0.10/', 'PSC')
+    psc_client = amieclient.Client(site_name='PSC')
+    psc_alt_base_client = amieclient.Client(site_name='PSC', base_url='https://amieclient.xsede.org/v0.20_beta/)
 
     """
-    def __init__(self, base_url, site_name):
+    def __init__(self, site_name, base_url='https://amieclient.xsede.org/v0.10/'):
         self.base_url = base_url
         self.site_name = site_name
+        self.full_url = urljoin(self.base_url, self.site_name)
+        self._session = ServerSession(self.full_url)
 
     @staticmethod
     def _join_list(things):
         if things is not None and things != []:
+            # If we're given a list, join it with commas
             return ','.join(things)
-        else:
+        elif things == []:
+            # if we're given an empty list, return None
             return None
+        else:
+            # If we're given anything else, i.e. None or some other single thing,
+            # give it back
+            return things
+
+    @staticmethod
+    def _dt_range(start, end):
+        if start is None and end is None:
+            time_str = None
+        else:
+            start_str = start.isoformat() if start else ""
+            end_str = end.isoformat() if end else ""
+            time_str = "{},{}".format(start_str, end_str)
+        return time_str
 
     def get_transaction(self, *, transaction_id):
         """
@@ -26,11 +61,41 @@ class Client(object):
         """
         pass
 
-    def transaction_list(self, *,
-                         transaction_ids=None, remote_sites=None,
-                         originating_sites=None, local_sites=None,
-                         update_time_start=None, update_time_until=None,
-                         transaction_state=None, incoming=True):
+    def list_packets(self, *, transaction_ids=None, outgoing=None,
+                     update_time_start=None, update_time_until=None,
+                     states=None, client_states=None, incoming=None):
+        """
+        Fetches a list of transactions based on the provided search parameters
+        """
+        transaction_ids_str = self._join_list(transaction_ids)
+        states_str = self._join_list(states)
+        client_states_str = self._join_list(client_states)
+        time_str = self._dt_range(update_time_start, update_time_until)
+
+        # Build a dict of parameters. Requests skips any with a None value,
+        # so no need to weed them out
+        params = {
+            'transaction_id': transaction_ids_str,
+            'outgoing': outgoing,
+            'update_time': time_str,
+            'states': states_str,
+            'client_states': client_states_str,
+            'incoming': incoming
+        }
+
+        # Get the list of packets
+        response = self._session.get('/packet_list', params=params)
+
+
+
+
+
+
+    def list_transactions(self, *,
+                          transaction_ids=None, remote_sites=None,
+                          originating_sites=None, local_sites=None,
+                          update_time_start=None, update_time_until=None,
+                          transaction_state=None, incoming=None):
         """
         Fetches a list of transactions based on the provided search parameters
         """
@@ -38,6 +103,7 @@ class Client(object):
         remote_sites_str = self._join_list(remote_sites)
         originating_sites_str = self._join_list(originating_sites)
         local_sites_str = self._join_list(local_sites)
+        time_str = self._dt_range(update_time_start, update_time_until)
 
     def send_packet(self, *, packet, transaction_id):
         pass
