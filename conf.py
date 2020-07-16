c5ad12d7cd5f7771e952cb3a4a6015a75b961469
G. Ryan Sablosky <sablosky@psc.edu>
2020-07-16 12:12:56 -0400

    add extensions

--- conf.py
+++ conf.py
@@ -28,6 +28,7 @@ author = 'G. Ryan Sablosky <sablosky@psc.edu>'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
