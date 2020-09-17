63b07603dcdb0ede9931e7bded47d1a6b2ddfc98
G. Ryan Sablosky <sablosky@psc.edu>
2020-09-17 14:23:34 -0400

    Add tests for getting a packet and a packet list

--- amieclient/test/test_client.py
+++ amieclient/test/test_client.py
@@ -1,9 +1,34 @@
 from ..client import Client
+from ..packet import RequestAccountCreate, PacketList
+from .fixtures import DEMO_JSON_PKT_1, DEMO_JSON_PKT_LIST
 
 
 class TestClient:
+
     def test_creation(self):
-        c = Client(site_name='test', api_key='test')
-        assert c
-        assert c._session.headers['XA-SITE'] == 'test'
-        assert c._session.headers['XA-API-KEY'] == 'test'
+        client = Client(site_name='test', api_key='test')
+        assert client._session.headers['XA-SITE'] == 'test'
+        assert client._session.headers['XA-API-KEY'] == 'test'
+
+    def test_get_packet(self, requests_mock):
+        client = Client(site_name='test', api_key='test')
+
+        # Make the mock request
+        packet_url = 'https://amieclient.xsede.org/v0.10/packets/test/12345'
+        requests_mock.get(packet_url, json=DEMO_JSON_PKT_1)
+
+        packet = client.get_packet(packet_rec_id='12345')
+        assert packet.packet_type == 'request_account_create'
+        assert isinstance(packet, RequestAccountCreate)
+
+    def test_get_packet_list(self, requests_mock):
+        client = Client(site_name='test', api_key='test')
+
+        # Make the mock request
+        packet_url = 'https://amieclient.xsede.org/v0.10/packets/test'
+        requests_mock.get(packet_url, json=DEMO_JSON_PKT_LIST)
+
+        r = client.list_packets()
+        assert isinstance(r, PacketList)
+        assert r.packets[0].packet_type == 'request_account_create'
+        assert r.packets[1].packet_type == 'request_account_create'
