5ab63200fef3d5f833a85e68a94c86042a722314
G. Ryan Sablosky <sablosky@psc.edu>
2021-02-05 14:38:36 -0500

    add a bit more metadata to the setup

--- setup.py
+++ setup.py
@@ -10,5 +10,16 @@ setup(
     ],
     author='G. Ryan Sablosky',
     author_email='sablosky@psc.edu',
-    python_requires=">=3.5",
+    python_requires='>=3.5',
+    description='Library for the XSEDE AMIE REST API.',
+    license='Apache Software License v2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
