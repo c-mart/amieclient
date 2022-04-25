1a5802794890a76df427311e99d3e395b7d743d8
G. Ryan Sablosky <sablosky@psc.edu>
2022-04-25 14:07:21 -0400

    v0.6.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='amieclient',
-    version='0.6.0',
+    version='0.6.1',
     packages=find_packages(),
     install_requires=[
         'requests>=2.20.0,<3',
